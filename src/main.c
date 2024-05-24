29ec08b5b6833ff1fbbf7a8b9ed41c16d28c0b0b
Ning <92511346+N1ngNi@users.noreply.github.com>
2024-05-25 01:41:35 +0700

    New command read method
    Im using token now to receive the command from user til user tab enter or space.
    currently working on font clear, video got a bit bug in CLI

--- src/main.c
+++ src/main.c
@@ -7,6 +7,7 @@
 #include "video.h"
 #include "fb.h"
 
+#define MAX_CMD_SIZE 100
 #define OS_NAME "Group2"
 #define IMAGE_WIDTH 1024
 #define IMAGE_HEIGHT 768
@@ -32,59 +33,170 @@ void TeamName()
     drawString(200, 400, "Huynh Ngoc Duy", 0x0000FF00, 3);    // Green
     drawString(200, 500, "Tran Thanh Tu", 0xFFFF0000, 3);     // Red
 }
+void commandList()
+{
+    uart_puts("\n1. Show font size\n");
+    uart_puts("   Please enter 'font'\n");
+    uart_puts("2. Show a small image\n");
+    uart_puts("   Please enter 'image'\n");
+    uart_puts("3. Show a larger image\n");
+    uart_puts("   Please enter 'image large'\n");
+    uart_puts("4. Show a video\n");
+    uart_puts("   Please enter 'video'\n");
+    uart_puts("5. Play a game\n");
+    uart_puts("   Please enter 'game'\n");
+    uart_puts("6. Clear screen\n");
+    uart_puts("   Please enter 'cls'\n");
+}
+//--------------New Receive Method-----------------//
+int cus_strcmp(const char *s1, const char *s2)
+{
+    while (*s1 && *s2 && *s1 == *s2)
+    {
+        ++s1;
+        ++s2;
+    }
+    return *s1 - *s2;
+}
+char *cus_strtok(char *str, const char *delim)
+{
+    static char *p = (char *)0;
+    if (*str != '\0')
+        p = str;
+
+    if (p == (char *)0 || *p == '\0')
+        return (char *)0;
+
+    char *start = p;
+
+    while (*p != '\0')
+    {
+        const char *d = delim;
+        while (*d != '\0')
+        {
+            if (*p == *d)
+            {
+                *p = '\0';
+                ++p;
+                if (*p == '\0')
+                    p = (char *)0;
+                return start;
+            }
+            ++d;
+        }
+        ++p;
+    }
+
+    return start;
+}
 
 void cli()
 {
-    //-----image-----//
-    char command = uart_getc();
-    if (command == UP)
+
+    static char cli_buffer[MAX_CMD_SIZE];
+    static int index = 0;
+    static char *command;
+    char c = uart_getc();
+    if (c != '\b')
     {
-        previousPosY = currentPosY;
-        currentPosY -= STEP; // scroll up by 10 pixels
-        deleteImage(currentPosX, previousPosY, SCREEN_SIZE, SCREEN_SIZE);
-        displayImage(currentPosX, currentPosY, image0, IMAGE_WIDTH, IMAGE_HEIGHT);
+        uart_sendc(c);
     }
-    else if (command == DOWN)
+    // put into a buffer until got new line or get backspace character
+    if (c != '\n' && c != '\b')
     {
-        previousPosY = currentPosY;
-        currentPosY += STEP; // scroll down by 10 pixels
-        deleteImage(previousPosX, previousPosY, SCREEN_SIZE, SCREEN_SIZE);
-        displayImage(currentPosX, currentPosY, image0, IMAGE_WIDTH, IMAGE_HEIGHT);
+        cli_buffer[index] = c; // Store into the buffer
+        index++;
     }
-    else if (command == LEFT)
+
+    // if user typein backspace it will delete the output and shift the index --
+    if (c == '\b')
     {
-        previousPosX = currentPosX;
-        currentPosX -= STEP; // scroll down by 10 pixels
-        deleteImage(previousPosX, previousPosY, SCREEN_SIZE, SCREEN_SIZE);
-        displayImage(currentPosX, currentPosY, image0, IMAGE_WIDTH, IMAGE_HEIGHT);
-    }
-    else if (command == RIGHT)
-    {
-        previousPosX = currentPosX;
-        currentPosX += STEP; // scroll down by 10 pixels
-        deleteImage(previousPosX, previousPosY, SCREEN_SIZE, SCREEN_SIZE);
-        displayImage(currentPosX, currentPosY, image0, IMAGE_WIDTH, IMAGE_HEIGHT);
-    }
-    else if (command == 'v')
-    {
-        deleteImage(previousPosX, previousPosY, SCREEN_SIZE, SCREEN_SIZE);
-        displayImage(350, 250, video1allArray[0], VID_WIDTH, VID_HEIGHT);
-        for (int i = 1; i < FRAME; i++)
+        if (index > 0)
         {
-            wait_msec(100000);
-            deleteImage(350, 250, VID_WIDTH, VID_HEIGHT);
-            displayImage(350, 250, video1allArray[i] , VID_WIDTH,  VID_HEIGHT);
+            uart_puts("\b \b");
+            index--;
+            cli_buffer[index] = '\0';
         }
     }
-}
 
+    else if (c == '\n')
+    {
+
+        cli_buffer[index] = '\0';
+        command = cus_strtok(cli_buffer, " ");
+
+        /* Compare with supported commands and execute
+         * ........................................... */
+        // HELP FUNCTION
+        if (cus_strcmp(command, "font") == 0)
+        {
+            TeamName();
+        }
+        else if (cus_strcmp(command, "video") == 0)
+        {
+            deleteImage(previousPosX, previousPosY, SCREEN_SIZE, SCREEN_SIZE);
+            displayImage(350, 250, video1allArray[0], VID_WIDTH, VID_HEIGHT);
+            for (int i = 1; i < FRAME; i++)
+            {
+                wait_msec(100000);
+                deleteImage(350, 250, VID_WIDTH, VID_HEIGHT);
+                displayImage(350, 250, video1allArray[i], VID_WIDTH, VID_HEIGHT);
+            }
+        }
+        else if (cus_strcmp(command, "clear") == 0)
+        {
+            fb_init(1920, 1080);
+            for (int i = 0; i < 1920; i++)
+            {
+                for (int j = 0; j < 1080; j++)
+                {
+                    drawPixelARGB32(i, j, 0x00000000);
+                }
+            }
+        }
+
+        //-----image-----//
+        if (command == UP)
+        {
+            previousPosY = currentPosY;
+            currentPosY -= STEP; // scroll up by 10 pixels
+            deleteImage(currentPosX, previousPosY, SCREEN_SIZE, SCREEN_SIZE);
+            displayImage(currentPosX, currentPosY, image0, IMAGE_WIDTH, IMAGE_HEIGHT);
+        }
+        else if (command == DOWN)
+        {
+            previousPosY = currentPosY;
+            currentPosY += STEP; // scroll down by 10 pixels
+            deleteImage(previousPosX, previousPosY, SCREEN_SIZE, SCREEN_SIZE);
+            displayImage(currentPosX, currentPosY, image0, IMAGE_WIDTH, IMAGE_HEIGHT);
+        }
+        else if (command == LEFT)
+        {
+            previousPosX = currentPosX;
+            currentPosX -= STEP; // scroll down by 10 pixels
+            deleteImage(previousPosX, previousPosY, SCREEN_SIZE, SCREEN_SIZE);
+            displayImage(currentPosX, currentPosY, image0, IMAGE_WIDTH, IMAGE_HEIGHT);
+        }
+        else if (command == RIGHT)
+        {
+            previousPosX = currentPosX;
+            currentPosX += STEP; // scroll down by 10 pixels
+            deleteImage(previousPosX, previousPosY, SCREEN_SIZE, SCREEN_SIZE);
+            displayImage(currentPosX, currentPosY, image0, IMAGE_WIDTH, IMAGE_HEIGHT);
+        }
+
+        uart_puts(OS_NAME);
+        uart_puts(":> ");
+        index = 0;
+    }
+}
 void main()
 {
     // set up serial console
     uart_init();
-
     // Initialize frame buffer with specific width and height
     fb_init(1024, 768);
+    // Initialize frame buffer
     // font
     TeamName();
     // Print OS line
