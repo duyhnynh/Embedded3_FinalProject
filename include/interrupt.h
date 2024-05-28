d7cdd5d6d3aeb73e8c6494022e503581ceb3b67a
Phan Trong Nguyen <phantnguyen2003@gmail.com>
2024-05-28 21:47:01 +0700

    change more

--- include/interrupt.h
+++ include/interrupt.h
@@ -4,18 +4,18 @@
 // #define MMIO_BASE 0x3F000000
 
 // Interrupt controller base address
-#define INTERRUPT_BASE (MMIO_BASE + 0xB000)
+#define REGS_IRQ (MMIO_BASE + 0xB000)
 
 // Interrupt controller registers for RPI3
-#define IRQ_PENDING_0 (*(volatile unsigned int *)(INTERRUPT_BASE + 0x200))
-#define IRQ_PENDING_1 (*(volatile unsigned int *)(INTERRUPT_BASE + 0x204))
-#define FIQ_CONTROL (*(volatile unsigned int *)(INTERRUPT_BASE + 0x20C))
+#define IRQ_PENDING_0 (*(volatile unsigned int *)(REGS_IRQ + 0x200))
+#define IRQ_PENDING_1 (*(volatile unsigned int *)(REGS_IRQ + 0x204))
+#define FIQ_CONTROL (*(volatile unsigned int *)(REGS_IRQ + 0x20C))
 #define INTERRUPT_ENABLE_IRQS                                                  \
-(*(volatile unsigned int *)(INTERRUPT_BASE + 0x210))
-#define ENABLE_BASIC_IRQS (*(volatile unsigned int *)(INTERRUPT_BASE + 0x218))
+(*(volatile unsigned int *)(REGS_IRQ + 0x210))
+#define ENABLE_BASIC_IRQS (*(volatile unsigned int *)(REGS_IRQ + 0x218))
 #define INTERRUPT_DISABLE_IRQS                                                 \
-  (*(volatile unsigned int *)(INTERRUPT_BASE + 0x21C))
-#define DISABLE_BASIC_IRQS (*(volatile unsigned int *)(INTERRUPT_BASE + 0x224))
+  (*(volatile unsigned int *)(REGS_IRQ + 0x21C))
+#define DISABLE_BASIC_IRQS (*(volatile unsigned int *)(REGS_IRQ + 0x224))
 
 // IRQ definitions
 #define SYS_TIMER_IRQ (1 << 1)
